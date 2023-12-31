1dd566bcac45d20f9ea68d0526025ae806876ab7
Abdul Haseeb <haseeb7063952@gmail.com>
2023-12-31 18:17:58 +0500

    update

--- assets/script/myScript.js
+++ assets/script/myScript.js
@@ -14,6 +14,22 @@ $('[data-fancybox="gallery"]').fancybox({
   
   'use strict';
 
+  // Go to Top Button Hide on Top of the Page
+
+const goTopBtn = document.getElementById("go-top-btn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    goTopBtn.style.display = "block";
+  } else {
+    goTopBtn.style.display = "none";
+  }
+});
+
+goTopBtn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
   /* global jQuery, PhotoSwipe, PhotoSwipeUI_Default, console */
   
   (function($) {
